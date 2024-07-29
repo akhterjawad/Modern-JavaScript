94506aaedadebe3590e64c2d5f4d556f5fefbf32
Muhammad Jawad <akhterjawad566@gmail.com>
2024-07-29 19:20:00 +0500

    20 % complete

---
+++ Async Await/app.js
@@ -0,0 +1,3 @@
+console.log('Async/Await');
+
+// Ham koi bhi normal function banata ha uska picha Async likhdata ha to wo Async function bunjata ha phir us function ka ander hum await likhdataha, await jis statement,line ka picha likhta ha to us line ko wait karna parta ha to next line pa move hojata ha jaisa 
