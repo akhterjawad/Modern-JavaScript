01267bf4344ab4c736adfd12023e1c54efb689ea
Muhammad Jawad <akhterjawad566@gmail.com>
2024-07-29 21:11:34 +0500

    Complete

---
+++ error Handling/app.js
@@ -0,0 +1,23 @@
+console.log('Error handling/try catch');
+
+// Callbacks
+// Promises
+// Async/Await
+// setTimeout
+// setInterval
+// setImmediate (Node.js)
+// process.nextTick (Node.js)
+// Event Listeners (e.g., click, load, readystatechange)
+// Fetch API
+// File API
+
+//try catch ka ander hum wo code likhta ha jis code ma hama doute ho  or catch ma hum error show karada ta ha. try ka ander hum asynchronous code nahi likhta jaisa settimeout balka asi situation ma hum asynchronous code ka andr try catch likhta ha
+
+
+console.log('hasnain');
+try {
+    console.log(jawad);
+} catch (error) {
+    console.log(error);
+}
+console.log('nana');
